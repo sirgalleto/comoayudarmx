ea095a5f9f348de3db8baacf61213802599c80ac
Patricio Beltran <pbelsal@gmail.com>
2017-09-20 15:56:33 -0700

    WIP: add form

--- modals.js
+++ modals.js
@@ -1,18 +1,65 @@
-$('.modal__link').click(function(e){
+$('.modal__link').click(function (e) {
   e.preventDefault();
   var modal = $(this).data('modal');
   $('.modal[data-modal="' + modal + '"]').addClass('show');
 })
 
-$('.modal__close').click(function(e){
+$('.modal__close').click(function (e) {
   e.preventDefault();
   $(this).parent().removeClass('show');
 })
 
 // Map
-$('.map-container').click(function() {
+$('.map-container').click(function () {
   $(this).find('iframe').addClass('clicked')
-})
-.mouseleave(function() {
+}).mouseleave(function () {
   $(this).find('iframe').removeClass('clicked')
+});
+
+$(function () {
+  var categories = ["Monetaria",
+    "Víveres",
+    "Artículos de limpieza",
+    "Artículos de aseo personal",
+    "Equipo de auxilio médico",
+    "Medicamentos",
+    "Sangre",
+    "Herramientas",
+    "Equipo de rescate",
+    "Albergue",
+    "Trabajo voluntario",
+    "Asesoría",
+    "Alojamiento",
+    "Veterinario"];
+
+  function split(val) {
+    return val.split(/,\s*/);
+  }
+  function extractLast(term) {
+    return split(term).pop();
+  }
+
+  $("#help_type").on("keydown", function (event) {
+    if (event.keyCode === $.ui.keyCode.TAB &&
+      $(this).autocomplete("instance").menu.active) {
+      event.preventDefault();
+    }
+  }).autocomplete({
+    minLength: 0,
+    source: function(request, response) {
+      response($.ui.autocomplete.filter(
+        categories, extractLast(request.term)));
+    },
+    focus: function () {
+      return false;
+    },
+    select: function (event, ui) {
+      var terms = split(this.value);
+      terms.pop();
+      terms.push(ui.item.value);
+      terms.push("");
+      this.value = terms.join(", ");
+      return false;
+    }
+  });
 });
